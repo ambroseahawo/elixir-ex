274449376f02632c7dc6093b2f2e3cab2c18254e
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-09-26 13:14:36 +0300

    Refactor controls

--- lib/controls/case_control.ex
+++ lib/controls/case_control.ex
@@ -4,12 +4,23 @@ defmodule Controls.CaseControl do
   """
   # @spec case_control(String) :: String
   def case_gender(gender) do
-    case gender do
-      "m" -> "Male"
-      "M" -> "Male"
-      "f" -> "Female"
-      "F" -> "Female"
-      _ -> "Undefined"
-    end
+    # case gender do
+    #   "m" -> "Male"
+    #   "M" -> "Male"
+    #   "f" -> "Female"
+    #   "F" -> "Female"
+    #   _ -> "Undefined"
+    # end
+
+    return_string =
+      case gender do
+        "m" -> "Male"
+        "M" -> "Male"
+        "f" -> "Female"
+        "F" -> "Female"
+        _ -> "Undefined"
+      end
+
+    "Could be #{return_string}"
   end
 end

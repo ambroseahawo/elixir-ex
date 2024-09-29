80a61aa0d37c1e1a8ed3b7dffb48c9a4b84e1e8d
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-09-29 21:48:38 +0300

    Implement sample tuple

---
+++ lib/tuples/tuples.ex
@@ -0,0 +1,12 @@
+defmodule Tuples do
+  @moduledoc """
+  tuples module demonstrating actions on a tuple
+  """
+
+  @doc """
+  return sample tuple
+  """
+  @spec sample_tuple() :: tuple()
+  def sample_tuple, do: {"Sam Altman", 33}
+
+end

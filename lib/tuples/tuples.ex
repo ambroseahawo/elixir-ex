cf9516c019f7597dddb607ec0cf315ee27c54084
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-09-29 22:29:46 +0300

    Update element in a tuple

--- lib/tuples/tuples.ex
+++ lib/tuples/tuples.ex
@@ -7,12 +7,18 @@ defmodule Tuples do
   return sample tuple
   """
   @spec sample_tuple() :: tuple()
-  def sample_tuple, do: {"Sam Altman", 33}
+  def sample_tuple, do: {:ok,"Sam Altman", 33}
 
   @doc """
   Accessing Elements in a tuple
   """
   @spec access_element(tuple(), integer()) :: any()
   def access_element(tuple_data \\ sample_tuple(), element_index), do: elem(tuple_data, element_index)
+
+  @doc """
+  Updating Elements in a tuple
+  """
+  @spec update_element(tuple(),integer(), any()) :: tuple()
+  def update_element(tuple_data \\ sample_tuple(), element_index, value), do: put_elem(tuple_data,element_index,value)
 
 end

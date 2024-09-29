f6507cea43dde52229fd958696ee5effcc2fc883
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-09-29 23:17:21 +0300

    Implement inserting element to tuple

--- lib/tuples/tuples.ex
+++ lib/tuples/tuples.ex
@@ -47,4 +47,25 @@ defmodule Tuples do
   # fallback clause for invalid inputs
   def update_element(_,_,_), do: {:error, "Invalid tuple"}
 
+  @doc """
+  Inserting into Tuple (Using Lists)
+  If no tuple is provided, defaults to `sample_tuple/0`.
+  """
+  @spec insert_element(tuple(),integer(),any()) :: {:ok, tuple()} | {:error, String.t()}
+  def insert_element(tuple_data \\ sample_tuple(), element_index, value)
+
+  # main clause that performs the insert
+  def insert_element(tuple_data, element_index, value) when is_tuple(tuple_data) do
+    list = Tuple.to_list(tuple_data)
+    if element_index <= length(list) and element_index >= 0 do
+      # {:ok, List.to_tuple(List.insert_at(list,element_index,value))}
+      {:ok, list |> List.insert_at(element_index, value) |> List.to_tuple}
+    else
+      {:error, "Index out of range"}
+    end
+  end
+
+  # fallback clause for invalid inputs
+  def insert_element(_,_,_), do: {:error, "Invalid tuple"}
+
 end
